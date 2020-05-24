e8ddc497364fd7faac23fe4f8b4e27576487d43b
austinlhx <aho103@syr.edu>
2020-05-24 02:18:39 -0700

    1.00

--- app.js
+++ app.js
@@ -1,33 +1,70 @@
 const discord = require("discord.js");
 const bot = new discord.Client();
-const twitter = require('twitter');
-const express = require("express.js");
+const twit = require('twit');
+const express = require("express");
 const app = express();
-const token = '';
+const token = 'INSERT TOKEN';
+const fs = require('fs');
+const https = require('https');
 
-const client = new twitter({
-    consumer_key: ' ',
-    consumer_secret: ' ',
-    access_token_key: ' ',
-    access_token_secret: ' '
-});
+var config = require('./config');
+const client = new twit(config);
 
-const params = {screen_name: 'nodejs'};
-
-app.set('port', (process.env.PORT || 5000));
-app.get(function (request, response){
-    var result = 'App is Running';
-}).listen(app.get('port'), function () {
-    console.log ('App is running, server is listening of port ' + app.get('port'));
-});
+var readyPost = false;
 
 bot.on("ready", () => {
-    console.log(`${bot.user.tag} has connected to the server`);
+    console.log(`${bot.user.tag} has connected to the server!`);
 });
 
 bot.on("message", msg => {
-    if (msg.channel.id === ' '){
+    if (msg.channel.id === 'Insert Channel ID'){
         if (msg.author.bot) return;
+        if (msg.attachments || msg.content){
+            const result = msg.attachments.map((res, i) => {
+                return res.proxyURL;
+            });
+            console.log(result);
+            const file = fs.createWriteStream("file.jpg");
+            const request = https.get(result[0], function(response) {
+            response.pipe(file);
+            });
+            console.log('File added');
+            var comment = msg.content;
+            
+
+            processing(comment);
+
+        }
 
     }
-})
+});
+
+
+function processing(content) {
+    var filename = 'file.jpg'
+    var b64content = fs.readFileSync(filename, { encoding: 'base64'});
+    client.post('media/upload', { media_data: b64content }, uploaded);
+    
+    function uploaded(err, data, response) {
+        var id = data.media_id_string;
+        var tweet = {
+            status: content,
+            media_ids: [id]
+        }
+        client.post('statuses/update', tweet, statusUpdate);
+    }
+
+    function statusUpdate(err, data, response) {
+        if (err) {
+             console.log(err);
+        }else{
+            console.log("Data posted");
+            fs.unlinkSync('file.jpg');
+            console.log('File Deleted');
+        }
+    }
+}
+
+
+
+bot.login(token);
