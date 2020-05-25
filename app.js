099c3b2bcb12df45f728a61b7d6f737f6690b1fc
austinlhx <aho103@syr.edu>
2020-05-25 15:21:14 -0700

    Initial Commit

--- app.js
+++ app.js
@@ -1,70 +1,132 @@
 const discord = require("discord.js");
 const bot = new discord.Client();
-const twit = require('twit');
-const express = require("express");
-const app = express();
-const token = 'INSERT TOKEN';
-const fs = require('fs');
-const https = require('https');
+const twit = require("twit");
+const token = "Insert Discord Token";
+const fs = require("fs");
 
-var config = require('./config');
+const axios = require("axios");
+const path = require("path");
+
+var config = require("./config");
 const client = new twit(config);
 
-var readyPost = false;
-
 bot.on("ready", () => {
-    console.log(`${bot.user.tag} has connected to the server!`);
+  console.log(`${bot.user.tag} has connected to the server!`);
 });
 
-bot.on("message", msg => {
-    if (msg.channel.id === 'Insert Channel ID'){
-        if (msg.author.bot) return;
-        if (msg.attachments || msg.content){
-            const result = msg.attachments.map((res, i) => {
-                return res.proxyURL;
-            });
-            console.log(result);
-            const file = fs.createWriteStream("file.jpg");
-            const request = https.get(result[0], function(response) {
-            response.pipe(file);
-            });
-            console.log('File added');
-            var comment = msg.content;
-            
+bot.on("message", (msg) => {
+  if (msg.channel.id === "Insert Channel ID") {
+    if (msg.author.bot) return;
 
-            processing(comment);
+    if (msg.attachments.size > 0) {
+      const result = msg.attachments.map((res, i) => {
+        return res.proxyURL;
+      });
 
-        }
+      var comment = msg.content;
+      
+      async function downloadImage() {
+        const url = result[0];
+        const file_dir = path.resolve(__dirname, "files", "success.png");
+        const writer = fs.createWriteStream(file_dir);
 
+        const response = await axios({
+          method: "GET",
+          url: url,
+          responseType: "stream",
+        });
+        response.data.pipe(writer);
+      }
+
+      downloadImage();
+
+      setTimeout(function () {
+        var b64 = fs.readFileSync("files/success.png", {
+          encoding: "base64",
+        });
+        client.post("media/upload", { media_data: b64 }, uploaded);
+      }, 5000);
+      
+     /*
+     async function downloadImage() {
+        const url = result[0];
+
+        const response = await axios({
+          method: "GET",
+          url: url,
+          responseType: "arrayBuffer",
+        });
+
+        const b64 = await Buffer.from(response.data, "utf-8");
+        client.post("media/upload", { media_data: b64 }, uploaded);
     }
-});
+    downloadImage();
+    */
 
-
-function processing(content) {
-    var filename = 'file.jpg'
-    var b64content = fs.readFileSync(filename, { encoding: 'base64'});
-    client.post('media/upload', { media_data: b64content }, uploaded);
-    
-    function uploaded(err, data, response) {
-        var id = data.media_id_string;
+      function uploaded(err, data, response) {
+          if (err) {
+              console.log(err);
+          }else{
+        var mediaIdStr = data.media_id_string;
         var tweet = {
-            status: content,
-            media_ids: [id]
-        }
-        client.post('statuses/update', tweet, statusUpdate);
+          status: `Success posted by ${msg.author.username} \n ${comment}`,
+          media_ids: [mediaIdStr],
+        };
+        client.post("statuses/update", tweet, statusUpdate);
     }
+      }
 
-    function statusUpdate(err, data, response) {
+      function statusUpdate(err, data, response) {
         if (err) {
-             console.log(err);
-        }else{
-            console.log("Data posted");
-            fs.unlinkSync('file.jpg');
-            console.log('File Deleted');
+          console.log(err);
+        } else {
+          //add embed reply
+          console.log(data);
+          var tweet_link =
+            "https://twitter.com/INSERT YOUR ACCOUNT HERE/status/" + data.id_str;
+          const embed = {
+            title: "Your Success has been posted!",
+            color: 8519796,
+            thumbnail: {
+              url: "Insert Custom Logo",
+            },
+            fields: [
+              {
+                name: "Click the 👎 to delete the tweet",
+                value: `You can view your tweet [here](${tweet_link})`,
+              },
+            ],
+            footer: {
+              text: "created by austin_hx#2583",
+            },
+            timestamp: new Date(),
+          };
+          msg.channel.send({ embed }).then((embedMessage) => {
+            embedMessage.react("👎");
+
+            const filter = (reaction, user) => {
+              return reaction.emoji.name === "👎" && user.id === msg.author.id;
+            };
+
+            embedMessage
+              .awaitReactions(filter, { max: 1 })
+
+              .then((collected) => {
+                client.post(
+                  "statuses/destroy/:id",
+                  { id: data.id_str },
+                  function (err, data, response) {
+                    console.log(data);
+                  }
+                );
+                embedMessage.delete();
+                msg.delete();
+              });
+          });
         }
+      }
     }
-}
-
-
+  }
+});
 
 bot.login(token);
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
     consumer_key: '',
     consumer_secret: '',
     access_token: '',
-    access_token_secret: ''
+    access_token_secret: '' //Insert Twitter keys here
 }
